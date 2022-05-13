c2385ddcde309e5e9b05d8aa24818bdafbf1fe17
zhraa sabah <zhraazhraahgz5@gmail.com>
2022-05-13 19:38:19 +0300

    first commit

---
+++ min.js
@@ -0,0 +1,11 @@
+  const navlink = document.getElementById("navlink")
+
+        function showmenu() {
+            navlink.style.right = "0"
+             navlink.style.display= "block"
+        }
+
+        function hidmenu() {
+            navlink.style.right = "-200px"
+             navlink.style.display= "none"
+        }
